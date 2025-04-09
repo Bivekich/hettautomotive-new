19b6e986dc83d3a41d672be3acffe137051541fa
Bivekich <biveki-dev@yandex.ru>
2025-04-09 23:35:14 +0300

    Удалены параметры конфигурации PM2 для упрощения настройки приложения. Эти изменения направлены на улучшение читаемости и поддерживаемости конфигурационного файла.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,10 +8,6 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 3002,
       },
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
     },
   ],
 }
